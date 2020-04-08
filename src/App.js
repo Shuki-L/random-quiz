ec9914dcdde6fa799371cca3092f583724d8762c
Shuki Levy <shukile@cisco.com>
2020-04-08 13:53:39 +0300

    Add quiz

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { questions, images } from "./constants";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  intervalID = 0;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageIndex: 0,
+      questionIndex: 0,
+      running: false,
+      started: false,
+    };
+  }
+
+  getRandomInt = (max) => {
+    return Math.ceil(Math.random() * Math.floor(max));
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  stop = () => {
+    clearInterval(this.interval);
+    this.setState({ running: false });
+  };
+
+  start = () => {
+    this.interval = setInterval(
+      () =>
+        this.setState({
+          imageIndex: this.getRandomInt(images.length - 1),
+          questionIndex: this.getRandomInt(questions.length - 1),
+          running: true,
+          started: true,
+        }),
+      100
+    );
+  };
+
+  render() {
+    const image = images[this.state.imageIndex];
+    const question = questions[this.state.questionIndex];
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/${image.path}`}
+            className="App-logo"
+            style={this.state.imageIndex === 43 ? { height: "500px" } : {}}
+            alt="logo"
+          />
+          {image.name && <p>{image.name}</p>}
+
+          {this.state.started && <p>{question}</p>}
+
+          <div id="menuBall1" className="menuBall">
+            {this.state.running && (
+              <button className="ball redball" onClick={this.stop}>
+                Stop
+              </button>
+            )}
+            {!this.state.running && (
+              <button className="ball greenball" onClick={this.start}>
+                start
+              </button>
+            )}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,49 @@
+export const images = [
+  { id: 0, path: "intro.png", name: "intro" },
+  { id: 1, path: "image1.png", name: "Robb Stark" },
+  { id: 2, path: "image2.jpeg", name: "Bronn" },
+  { id: 3, path: "image3.png", name: "Samwell Tarly" },
+  { id: 4, path: "image4.png", name: "Jon Snow" },
+  { id: 5, path: "image5.jpeg", name: "Daenerys Targaryen" },
+  { id: 6, path: "image6.png", name: "Missandei" },
+  { id: 7, path: "image7.jpeg", name: "Zoti and Zoti" },
+  { id: 8, path: "image8.png", name: "Voldemort" },
+  { id: 9, path: "image9.jpeg", name: "Harry potter" },
+  { id: 10, path: "image10.png", name: "Ron Weasley" },
+];
+
+export const questions = [
+  "Question #1",
+  "Question #2",
+  "Question #3",
+  "Question #4",
+  "Question #5",
+  "Question #6",
+  "Question #7",
+  "Question #8",
+];
+
+//COVID19 questions
+
+// export const questions = [
+//   "I'll break quarantine only for ______",
+//   "Working from home is awesome because ______",
+//   "The first thing I'm going to do when the Covid-19 lockdown is over",
+//   "Weired habit that you developed in quarantine",
+//   "Good habit that you developed in quarantine",
+//   "Bad habit that you developed in quarantine",
+//   "One person (from all over the world) I would like to be in quarantine with",
+//   "One person (from the office) I would like to be in quarantine with",
+//   "The SHABAC put an eye on us- what can they tell us about you that we don't already know?",
+//   "The first destination I'll fly to when sky will be open again",
+//   "What do you miss the most at the office?",
+//   "What don't you miss at the office?",
+//   "When we back to the office-what is the first restaurant you'll go for lunch?",
+//   "What is the first thing you cleaned in your house?",
+//   "What do you tell yourself you'll do after the quarantine, and know you're not really going to do that?",
+//   "The one thing I missed the most is _____",
+//   "What was the best meal you cooked in quarantine?",
+//   "What cheers you up in this hard times?",
+//   "What is the best TV show/series you watched in quarantine?",
+//   "I have a question for you!!"
+// ];
