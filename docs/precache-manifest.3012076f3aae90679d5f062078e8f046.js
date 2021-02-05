eeb07ede35876b8bee69200375b65cd0aafd2ae3
Shuki Levy <shukile@cisco.com>
2021-02-06 00:41:22 +0200

    add docs

---
+++ docs/precache-manifest.3012076f3aae90679d5f062078e8f046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50af6e90873ca8619d6895ce5c889fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c4125b1fe1f308b87f9",
+    "url": "/static/css/main.5eb0cbd4.chunk.css"
+  },
+  {
+    "revision": "44afbfc2c511181cea5c",
+    "url": "/static/js/2.953ed3ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.953ed3ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c4125b1fe1f308b87f9",
+    "url": "/static/js/main.1ada4761.chunk.js"
+  },
+  {
+    "revision": "91638c1a9b1891fc09e8",
+    "url": "/static/js/runtime-main.0daf2364.js"
+  }
+]);
